26e66e211ac48c08fec59b28e94740736e3ed14b
Filipp Tarkovich <70907211+fill-tarkovich@users.noreply.github.com>
2023-02-20 18:03:54 +0200

    exercise 1.6

---
+++ Part1/unicafe/src/App.js
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import Button from "./Button";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const handleGoodClick = () => {
+    setGood(good + 1)
+  }
+
+   const handleNeutralClick = () => {
+    setNeutral(neutral + 1)
+  }
+
+   const handleBadClick = () => {
+    setBad(bad + 1)
+  }
+
+  return <div>
+    <h2>give feedback</h2>
+    <div>
+      <Button handleClick={handleGoodClick} text={"good"}></Button>
+      <Button handleClick={handleNeutralClick} text={"neutral"}></Button>
+      <Button handleClick={handleBadClick} text={"bad"}></Button>
+    </div>
+    <h2>statistics</h2>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+  </div>;
+};
+
+export default App;
