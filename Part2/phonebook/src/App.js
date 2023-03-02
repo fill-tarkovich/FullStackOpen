f67fc48ceff37118ca4844d2f69d6b92d4f43fc5
Filipp Tarkovich <70907211+fill-tarkovich@users.noreply.github.com>
2023-03-03 01:08:11 +0200

    Exercise 2.9

--- Part2/phonebook/src/App.js
+++ Part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filtered, setFiltered] = useState(persons);
 
   const addPerson = (event) => {
     event.preventDefault();
@@ -11,7 +17,7 @@ const App = () => {
       name: newName,
       number: newNumber,
     };
-    JSON.stringify(persons).includes(JSON.stringify(newPerson))
+    JSON.stringify(persons).includes(JSON.stringify(newPerson.name))
       ? alert(`${newName} is already added to phonebook`)
       : setPersons(persons.concat(newPerson));
     setNewName("");
@@ -26,9 +32,21 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const applyFilter = (event) => {
+    const value = event.target.value;
+    const filteredPersons = persons.filter((person) =>
+      JSON.stringify(person.name).toLowerCase().includes(value.toLowerCase())
+    );
+    setFiltered(filteredPersons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={applyFilter} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -41,7 +59,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filtered.map((person) => (
         <li key={person.name}>
           {person.name} {person.number}
         </li>
