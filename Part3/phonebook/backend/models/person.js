4234d0e13537fa789d3a4b21cc3846abd1efd859
Filipp Tarkovich <70907211+fill-tarkovich@users.noreply.github.com>
2023-03-26 01:55:23 +0200

    Exercise 3.22

--- Part3/phonebook/backend/models/person.js
+++ Part3/phonebook/backend/models/person.js
@@ -1,41 +1,41 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-mongoose.set("strictQuery", false);
+mongoose.set('strictQuery', false)
 
-const url = process.env.MONGODB_URL;
+const url = process.env.MONGODB_URL
 
-console.log("connecting to", url);
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-  name: { type: String, minLength: 5, required: [true, "Name is missing"] },
+  name: { type: String, minLength: 5, required: [true, 'Name is missing'] },
   number: {
     type: String,
     minLength: 8,
-    required: [true, "Phone number is missing"],
+    required: [true, 'Phone number is missing'],
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{5,6}/.test(v);
+        return /\d{2,3}-\d{5,6}/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
   },
-});
+})
 
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
